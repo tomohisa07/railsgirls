5570890e487fb7bdd249f2e2e1bb4b243bd463eb
sakutomo07 <arai15735320@gmail.com>
2020-03-18 08:12:39 +0900

    lesson11
    finish lesson11

---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Idea, type: :model do
+  it 'has a name' do # yep, you can totally use 'it'
+    idea = Idea.create!(name: 'My Awesome Idea Name') # creating a new idea 'instance'
+    second_idea = Idea.create!(name: 'My Second Idea Name') # creating another new idea 'instance'
+    expect(second_idea.name).to eq('My Second Idea Name') # this is our expectation
+  end
+end
